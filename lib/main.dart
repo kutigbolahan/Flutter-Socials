e4b53b320e38fea79898a5ffd8bca28cf3afb7cd
kuti gbolahan <kutigbolahan@ymail.com>
2020-03-17 13:27:22 +0100

    Changes to be committed:

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:socials/views/feed_screen.dart';
 import 'package:socials/views/login_screen.dart';
 import 'package:socials/views/sign_up_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  Widget _getScreenId() {
+    return StreamBuilder<FirebaseUser>(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return FeedScreen();
+          } else {
+            return LoginScreen();
+          }
+         // snapshot.hasData ? FeedScreen() : LoginScreen();
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: LoginScreen(),
       routes: {
-        LoginScreen.routeName :(context) => LoginScreen(),
-        SignUpScreen.routeName :(context) => SignUpScreen(),
+        LoginScreen.routeName: (context) => LoginScreen(),
+        SignUpScreen.routeName: (context) => SignUpScreen(),
       },
     );
   }
 }
-
